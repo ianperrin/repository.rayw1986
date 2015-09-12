f6f84eeb3ca345d685b5834c0770a71b5a338e93
RayW1986 <raywilson1986@hotmail.co.uk>
2015-09-12 12:11:45 +0100

    Pushed version 1.1.1

--- context.genesis/addon.py
+++ context.genesis/addon.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-		
+
+'''
+    Genesis Context Menu Tools Add-on
+    Copyright (C) 2015 rayw1986
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 		
 import xbmc,xbmcaddon,xbmcgui		
 addon = xbmcaddon.Addon('plugin.video.genesis')
@@ -42,7 +60,21 @@ def imdb_tv_watchlist():
 		
 def search():
 		xbmc.executebuiltin('XBMC.RunAddon(plugin.video.genesis,action=searchNavigator)')
-
+		
+def import_options():
+		dialog = xbmcgui.Dialog()
+		funcs = (
+			trakt_collection,
+			trakt_watchlist,
+			trakt_tv_collection,
+			trakt_tv_watchlist,
+			imdb_watchlist,
+			imdb_tv_watchlist,
+		)
+		ret = dialog.select('Genesis Import Options', ['[B]TRAKT[/B] : Import Collection...', '[B]TRAKT[/B] : Import Watchlist...', '[B]TRAKT[/B] : Import TV Collection...', '[B]TRAKT[/B] : Import TV Watchlist...', '[B]IMDB[/B] : Import Watchlist...', '[B]IMDB[/B] : Import TV Watchlist...'])
+		if ret > -1:
+			funcs[ret]()
+			
 if __name__ == '__main__':
 		dialog = xbmcgui.Dialog()
 		funcs = (
@@ -51,14 +83,9 @@ if __name__ == '__main__':
 			clear_cache,
 			clear_sources,
 			update_folders,
-			trakt_collection,
-			trakt_watchlist,
-			trakt_tv_collection,
-			trakt_tv_watchlist,
-			imdb_watchlist,
-			imdb_tv_watchlist,
-			search
+			search,
+			import_options
 		)
-		ret = dialog.select('Genesis Library Options', ['Genesis Settings', 'Toggle Autoplay', 'Clear Cache...', 'Clear Sources...', 'Update Folders...', '[B]TRAKT[/B] : Import Collection...', '[B]TRAKT[/B] : Import Watchlist...', '[B]TRAKT[/B] : Import TV Collection...', '[B]TRAKT[/B] : Import TV Watchlist...', '[B]IMDB[/B] : Import Watchlist...', '[B]IMDB[/B] : Import TV Watchlist...', 'Search Genesis'])
+		ret = dialog.select('Genesis Library Options', ['Genesis Settings', 'Toggle Autoplay', 'Clear Cache', 'Clear Sources', 'Update Folders', 'Search Genesis', 'Library Import Options...'])
 		if ret > -1:
 			funcs[ret]()
